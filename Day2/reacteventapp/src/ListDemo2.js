2d7174dad1256ba09e68a11489a9e9ecee7eee1f
hminah0215 <hyeonminah@gmail.com>
2021-07-12 14:30:45 +0900

    이벤트 처리

--- Day2/reacteventapp/src/ListDemo2.js
+++ Day2/reacteventapp/src/ListDemo2.js
@@ -34,6 +34,7 @@ const ListDemo2 = () => {
     console.log(newProduct);
   };
 
+  // 저장버튼 이벤트
   const onSaveProduct = (e) => {
     // 배열.concat() 메소드는 배열에 신규아이템을 추가할때 사용한다.
     const newProductList = products.concat({ ...product, pid: nextId });
@@ -43,6 +44,36 @@ const ListDemo2 = () => {
 
     // 다음번호 미리 생성하기
     setNextId(nextId + 1);
+  };
+
+  // 테이블에 있는 제품 삭제버튼 이벤트
+  const onRemoveProduct = (pid) => {
+    // 제품목록에서 전달된 pid 값이 아닌 목록만 조회해서 제품목록을 다시 업데이트한다.
+    // 배열.filter()메소드는 해당 조건값만 배열을 필터해서 조회목록을 반환한다.
+    // 전달된 pid 값을 제외한 목록을 반환한다.
+    const filteredProductList = products.filter((product) => product.pid !== pid);
+
+    // 제품목록을 갱신한다.
+    setProducts(filteredProductList);
+  };
+
+  // 더블클릭 이벤트
+  const onSelectProduct = (item) => {
+    // console.log("선택pid값", pid);
+    // const seletedProduct = products.filter((item) => item.pid === pid);
+
+    // 단일 제품정보 업데이트
+    setProduct(item);
+  };
+
+  // 수정 버튼 클릭 이벤트
+  const onModifyProduct = (item) => {
+    // 현재 선택된 아이템을 목록에서 제거하고
+    const filteredProductList = products.filter((item) => item.pid !== product.pid);
+    const resultlist = filteredProductList.concat(product);
+
+    console.log("수정된 아이템 ===>", resultlist);
+    setProducts(resultlist);
   };
 
   return (
@@ -65,7 +96,8 @@ const ListDemo2 = () => {
       재고수량 :
       <input type="text" name="stock" value={product.stock} onChange={onProductChange} />
       <br></br>
-      <button onClick={onSaveProduct}>저장</button>
+      <button onClick={onSaveProduct}>추가</button>
+      <button onClick={onModifyProduct}>수정</button>
       <hr />
       <table>
         <thead>
@@ -75,6 +107,7 @@ const ListDemo2 = () => {
             <th>공급업체</th>
             <th>가격</th>
             <th>재고수량</th>
+            <th>데이터삭제</th>
           </tr>
         </thead>
         <tbody>
@@ -82,12 +115,15 @@ const ListDemo2 = () => {
           {/* map을 사용해서 ui를 반복표현하는 경우 반드시 key 속성에 유니크한 값을 설정해줘야한다.<tr key={product.pid}> 
           동일 컴포넌트내에서 배열의 키 설정값은 중복되면 안된다! unique!! */}
           {products.map((product) => (
-            <tr key={product.pid}>
+            <tr key={product.pid} onDoubleClick={() => onSelectProduct(product)}>
               <td>{product.pid}</td>
               <td>{product.productName}</td>
               <td>{product.provider}</td>
               <td>{product.price}</td>
               <td>{product.stock}</td>
+              <td>
+                <button onClick={() => onRemoveProduct(product.pid)}>삭제</button>
+              </td>
             </tr>
           ))}
         </tbody>
