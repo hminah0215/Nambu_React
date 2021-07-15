68c6351fb765e460ae30867405015033f1d37d1a
hminah0215 <hyeonminah@gmail.com>
2021-07-15 16:19:55 +0900

    로그인 미션 기본구조

---
+++ Day6/loginapp/src/components/TopMenu.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const TopMenu = () => {
+  return (
+    <div style={{ width: "100%", height: "50px", background: "gray" }}>
+      <span>
+        <Link to="/">홈</Link>
+      </span>
+      &nbsp;
+      <span>
+        <Link to="/member/list">회원 목록</Link>
+      </span>
+      &nbsp;
+      <span>
+        <Link to="/member/login">로그인</Link>
+      </span>
+    </div>
+  );
+};
+
+export default TopMenu;
